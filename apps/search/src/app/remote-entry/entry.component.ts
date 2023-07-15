12ab63d09947921aceb9d2736472e909f256b0ee
Amit775 <38419204+Amit775@users.noreply.github.com>
2023-07-15 17:03:39 +0300

    with webcomponent

--- apps/search/src/app/remote-entry/entry.component.ts
+++ apps/search/src/app/remote-entry/entry.component.ts
@@ -1,36 +1,49 @@
 import { CommonModule } from '@angular/common';
 import {
+	CUSTOM_ELEMENTS_SCHEMA,
   Component,
   EventEmitter,
   Injector,
   Input,
+  OnInit,
   Output,
   ViewEncapsulation,
   inject,
 } from '@angular/core';
-import { StoreService } from '@ng-mf/shared/store';
 import { SearchComponent } from '../search/search.component';
-import { STORE_SERVICE } from '../token';
+import { createCustomElement } from '@angular/elements';
 
 @Component({
   standalone: true,
   imports: [CommonModule, SearchComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   selector: 'ng-mf-search-entry',
   template: `<ng-mf-search
     [text]="text"
     (check)="emit($event)"
-  ></ng-mf-search>`,
+  ></ng-mf-search><wng-mf-search
+    [text]="text"
+    (check)="emitElement($event)"
+  ></wng-mf-search>`,
   encapsulation: ViewEncapsulation.ShadowDom,
 })
-export class RemoteEntryComponent {
+export class RemoteEntryComponent implements OnInit {
   @Input() text?: string;
   @Output() check = new EventEmitter<string>();
 
-  
   injector: Injector = inject(Injector);
-  
 
+  ngOnInit(): void {
+	const element = createCustomElement(SearchComponent, { injector: this.injector })
+	customElements.define('wng-mf-search', element);
+  }
+
+  emitElement(event: Event): void {
+	const custommEvent: CustomEvent = event as CustomEvent;
+	this.emit(custommEvent.detail);
+  }
   emit(text: string): void {
+	console.log(text);
     this.check.emit(text);
   }
 }
---
+++ apps/search/src/app/webcomponent-entry/webcomponent-entry.module.ts
@@ -0,0 +1,12 @@
+// import { ApplicationRef, DoBootstrap, Injector, NgModule, inject } from "@angular/core";
+// import { SearchComponent } from "../search/search.component";
+// import { createCustomElement } from "@angular/elements";
+
+// @NgModule()
+// export class WebcomponentEntryModule implements DoBootstrap {
+// 	injector = inject(Injector);
+
+// 	ngDoBootstrap(appRef: ApplicationRef): void {
+
+// 	}
+// }
